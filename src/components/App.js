ea867dbb63bc035891dc5d354370f2a235be8ef1
Barbara <barbara.estevez@gmail.com>
2021-07-30 21:04:58 +0200

    add api service file
    add loadCities component

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+
+import '../stylesheets/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Cities
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
